288832a9991928ad6334b9ceb771f4a67f33d249
mayarmohsen766-commits <mayarmohsen766@gmail.com>
2026-03-18 23:08:54 +0200

    ITEM CLASS

--- Mayar_Mohsen.cpp
+++ Mayar_Mohsen.cpp
@@ -1,1 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
 
+// ============================================================
+// ITEM CLASS
+// ============================================================
+class Item
+{
+protected:
+    string name;
+    string type;
+
+public:
+    Item(string n, string t) : name(n), type(t) {}
+    string getName() { return name; }
+    string getType() { return type; }
+    virtual void use(class Character *target) = 0;
+    virtual void showInfo() { cout << "[" << type << "] " << name; }
+    virtual ~Item() {}
+};
